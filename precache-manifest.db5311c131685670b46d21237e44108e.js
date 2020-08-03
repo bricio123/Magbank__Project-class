c960b669479fe093e6d3198e718c5375a6b2a3b8
Fabricio sousa <fabricio1234c@gmail.com>
2020-08-03 20:51:37 -0300

    Updates

---
+++ precache-manifest.db5311c131685670b46d21237e44108e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f50b677b676ff38a840b0d3595228cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d430d9ec50674554d8b",
+    "url": "/static/css/main.95b0cd29.chunk.css"
+  },
+  {
+    "revision": "ca73ac5c093bf2706d63",
+    "url": "/static/js/2.97654f77.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.97654f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d430d9ec50674554d8b",
+    "url": "/static/js/main.511e11c7.chunk.js"
+  },
+  {
+    "revision": "01f394937e45d5eb8f13",
+    "url": "/static/js/runtime-main.c27e7200.js"
+  },
+  {
+    "revision": "2f93cedd9218f692781cfd475a03b5ae",
+    "url": "/static/media/credit-card.2f93cedd.jpg"
+  },
+  {
+    "revision": "f992ac6610598c0169cce6b7fac496b6",
+    "url": "/static/media/hero.f992ac66.jpg"
+  },
+  {
+    "revision": "553335da82c3f10ce54b880974826e46",
+    "url": "/static/media/institutional.553335da.jpg"
+  },
+  {
+    "revision": "f4a4c7d05e2ec15313d07f737273d9d2",
+    "url": "/static/media/logo.f4a4c7d0.svg"
+  },
+  {
+    "revision": "6620cf10db2ef9f74cbb3fea1201729e",
+    "url": "/static/media/logo__magic-pay--mobile.6620cf10.png"
+  },
+  {
+    "revision": "f334775a0fce8a2f32042ccbc4d83d48",
+    "url": "/static/media/logo__magic-pay.f334775a.png"
+  },
+  {
+    "revision": "589c16b82888f5a5cebe46497e721c67",
+    "url": "/static/media/post-3.589c16b8.jpg"
+  }
+]);
